6e42c9f13b91d7620f056c13f54517b82d8181b9
唐洪峰 <thfhongfeng@163.com>
2024-08-12 17:39:29 +0800

    update

---
+++ tool/src/main/java/com/pine/tool/router/IResponseBundleKey.java
@@ -0,0 +1,17 @@
+package com.pine.tool.router;
+
+/**
+ * Created by tanghongfeng on 2018/9/12
+ */
+
+public interface IResponseBundleKey {
+    String ACTION_METHOD = "actionMethod";
+
+    String METHOD_DATA = "methodData";
+    String METHOD_DATA1 = "methodData1";
+    String METHOD_DATA2 = "methodData2";
+    String METHOD_DATA3 = "methodData3";
+    String METHOD_DATA4 = "methodData4";
+    String METHOD_DATA5 = "methodData5";
+    String METHOD_DATA6 = "methodData6";
+}
